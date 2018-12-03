bb093b412a1168c0e0f7537c97b89802ddc955ec
buniello <30833755+buniello@users.noreply.github.com>
2018-12-03 15:51:20 +0000

    Update utils.py

--- curator-formatter/format/utils.py
+++ curator-formatter/format/utils.py
@@ -34,6 +34,7 @@ known_header_transformations = {
     'chromosome': 'chr',
     'chrom': 'chr',
     'scaffold': 'chr',
+    'chr_build36': 'chr'
     # base pair location
     'bp': 'bp',
     'pos': 'bp',
@@ -42,6 +43,7 @@ known_header_transformations = {
     'base_pair': 'bp',
     'basepair': 'bp',
     'base_pair_location': 'bp',
+    'pos_build36':'bp',  
     # chromosome combined with base pair location
     'chr_pos' : 'chr_bp',
     'chrpos' : 'chr_bp',
