1e8539b96204707af6a10bad28e785c837b8aa92
jdhayhurst <38317975+jdhayhurst@users.noreply.github.com>
2019-03-21 13:12:14 +0000

    Update setup.py

--- curator-formatter/setup.py
+++ curator-formatter/setup.py
@@ -24,5 +24,5 @@ setup(
     license='',
     author='Olga Vrousgou',
     author_email='olgavrou@gmail.com',
-    install_requires=['tqdm', 'pandas', 'numpy']
+    install_requires=['tqdm', 'pandas', 'numpy>=1.12.0']
 )
